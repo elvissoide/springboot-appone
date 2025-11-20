8d39f4565c8bebb81dba01642bbfbdb892b3318c
Elvis Guanoluisa <elviserock@gmail.com>
2025-11-20 14:48:51 -0500

    Initialize Spring Boot application with Maven wrapper and necessary configurations

---
+++ src/main/java/com/ecoders/spring/appone/springboot_appone/SpringbootApponeApplication.java
@@ -0,0 +1,13 @@
+package com.ecoders.spring.appone.springboot_appone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApponeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApponeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoders/spring/appone/springboot_appone/SpringbootApponeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoders.spring.appone.springboot_appone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApponeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
