df64b353919f72b22353d9285a54622ff1e071eb
Elvis Guanoluisa <elviserock@gmail.com>
2025-12-01 15:31:47 -0500

    Add detalles_info.html and EjemploController for basic Spring Boot setup

---
+++ src/main/java/com/ecoders/spring/appone/springboot_appone/controllers/EjemploController.java
@@ -0,0 +1,14 @@
+package com.ecoders.spring.appone.springboot_appone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EjemploController {
+
+    @GetMapping("/detalles_info")
+    public String info(){
+        return "detalles_info";
+    }
+
+}
