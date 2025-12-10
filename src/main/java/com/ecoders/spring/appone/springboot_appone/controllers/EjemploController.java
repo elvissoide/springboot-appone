2c614c89237967300b691219d5483f571d5092cb
Elvis Guanoluisa <elviserock@gmail.com>
2025-12-10 15:00:01 -0500

    Refactor EjemploController to pass model attributes and update detalles_info.html to use Thymeleaf expressions

--- src/main/java/com/ecoders/spring/appone/springboot_appone/controllers/EjemploController.java
+++ src/main/java/com/ecoders/spring/appone/springboot_appone/controllers/EjemploController.java
@@ -1,13 +1,17 @@
 package com.ecoders.spring.appone.springboot_appone.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String info(){
+    public String info(Model model) {
+        model.addAttribute("Titulo", "Servidor en linea con Spring Boot");
+        model.addAttribute("Detalle", "Aplicacion de ejemplo para Spring Boot");
+        model.addAttribute("Servidor", "Servidor dex desarrollado por Ecoders");
         return "detalles_info";
     }
 
